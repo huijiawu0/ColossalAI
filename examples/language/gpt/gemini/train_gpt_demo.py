986094211fda6b2c421aed3eec44602f13e5db99
huijiawu0 <3816911+huijiawu0@users.noreply.github.com>
2023-02-27 23:15:02 +0800

    fix add

--- examples/language/gpt/gemini/train_gpt_demo.py
+++ examples/language/gpt/gemini/train_gpt_demo.py
@@ -379,8 +379,8 @@ def main():
                     f"[EVAL] [{n + 1}/{NUM_STEPS}] Val loss:{vloss['val'].item():.3f}",
                     ranks=[0],
                 )
-                if vloss['val'] < best_val_loss:
-                    best_val_loss = vloss['val']
+                if vloss['val'].item() < best_val_loss:
+                    best_val_loss = vloss['val'].item()
                     logger.info("save model to out...")
                     save_checkpoint('out', 0, model)
 
